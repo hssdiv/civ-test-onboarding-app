7fdbf91cb2cdb6224198f32f8c602ae4330fd736
aleksey.driuk <AlexDruyk@ukr.net>
2025-07-13 13:20:33 +0300

    added toast lib

--- App.tsx
+++ App.tsx
@@ -3,14 +3,24 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { RootStack } from './src/navigation/root.stack';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
+import Toast from 'react-native-toast-message';
+import { useGetToastConfig } from './src/helper';
 
 export default function App() {
+  const toastConfig = useGetToastConfig();
+
   return (
     <GestureHandlerRootView>
       <NavigationContainer>
         <StatusBar style="auto" />
         <View style={styles.container}>
           <RootStack />
+          <Toast
+            visibilityTime={5000}
+            position="bottom"
+            config={toastConfig}
+            onPress={() => Toast.hide()}
+          />
         </View>
       </NavigationContainer>
     </GestureHandlerRootView>
