68b1183ed8b252a3a7bcd501b851cce35078aa87
aleksey.driuk <AlexDruyk@ukr.net>
2025-07-13 07:23:22 +0300

    colors naming refactor

--- src/styles/palette.ts
+++ src/styles/palette.ts
@@ -1,13 +1,7 @@
 export const palette = {
   red: '#dc3545',
-  blueMain: 'rgba(44, 20, 221, 1)',
   blueSecondary: 'rgba(44, 20, 201, 1)',
-  pink: 'rgba(229, 180, 255, 1)',
-  pinkSecondary: 'rgba(229, 180, 235, 1)',
   white: 'white',
   black: 'black',
-  backgroundPrimary: 'rgba(245, 247, 255, 1)',
-  backgroundDark: 'rgba(36, 15, 81, 1)',
-  backgroundSecondaryDark: 'rgba(58, 39, 98, 1)',
-  buttonSmallDark: 'rgba(80, 63, 116, 1)',
+  primaryLight: 'rgba(44, 20, 221, 1)', // '#2C14DD'
 };
