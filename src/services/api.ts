bcd2a3e6577c26af6edd0ef61b9f7209c084470b
aleksey.driuk <AlexDruyk@ukr.net>
2025-07-14 23:57:12 +0300

    added api retry

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,21 @@
 import axios from "axios";
+import axiosRetry from 'axios-retry';
+
 import { env } from "../../env.config";
 
 export const api = axios.create({
   baseURL: `${env.API_URL}`,
 });
-api.defaults.timeout = 10000;
+
+axiosRetry(api, {
+  retries: 3,
+  retryDelay: retryCount => retryCount * 1000,
+  shouldResetTimeout: true,
+  retryCondition: (error) => {
+    return axiosRetry.isNetworkError(error) || error.code === 'ECONNABORTED';
+  },
+  onRetry: (retryCount, error, requestConfig) => {
+    console.log(`Retry attempt #${retryCount} for ${requestConfig.url}`);
+  },
+});
+api.defaults.timeout = 5000;
