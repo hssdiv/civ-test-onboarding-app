8dfff413a6fdbd010917e939af9c32b108bdfa66
aleksey.driuk <AlexDruyk@ukr.net>
2025-07-15 09:41:41 +0300

    updated formatprice

--- src/navigation/app/account/helper/formatPrice.ts
+++ src/navigation/app/account/helper/formatPrice.ts
@@ -1,5 +1,5 @@
 export const formatPrice = (price?: number) => {
-  if (!price || !Number.isInteger(price)) return '';
+  if (!price || Number.isNaN(Number(price))) return '';
   const priceString = price.toFixed(2); // in some places price has ".00" at the end, in others not, prob need to add it everywhere as it looks "right" that way
   return priceString.replace(/\B(?=(\d{3})+(?!\d))/g, ",");;
 };
